99d3adfb22f18033c3c49916cea23ee363223781
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-06-22 22:48:50 +0530

    largest nu. check

---
+++ Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10, b = 25, c = 15;
+        int max = Math.max(a, Math.max(b, c));
+        System.out.println("Largest = " + max);
+    }
+}
