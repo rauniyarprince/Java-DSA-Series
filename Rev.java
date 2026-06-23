3d0ada7df2cc858a867cea9eac75ffa901c5c740
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-06-23 22:27:35 +0530

    reverse

---
+++ Rev.java
@@ -0,0 +1,12 @@
+public class Rev {
+  
+}
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Java";
+        
+        for(int i = str.length() - 1; i >= 0; i--) {
+            System.out.print(str.charAt(i));
+        }
+    }
+}
