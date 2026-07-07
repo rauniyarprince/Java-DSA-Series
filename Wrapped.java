9490c58c5664867442727d8f7cc55bdbbaaa614a
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-07 21:23:04 +0530

    wrapped ex

---
+++ Wrapped.java
@@ -0,0 +1,12 @@
+class Geeks {
+    public static void main(String[] args) {
+
+        int b = 357;
+        
+        // Autoboxing: primitive int -> Integer object
+        Integer a = b;
+
+        System.out.println("The primitive int b is: " + b);
+        System.out.println("The Integer object a is: " + a);
+    }
+}
