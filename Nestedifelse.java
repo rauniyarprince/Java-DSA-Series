369de0a1954bd23deeb295954023b5b194927c5c
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-10 22:02:18 +0530

    update the code

--- Nestedifelse.java
+++ Nestedifelse.java
@@ -11,6 +11,7 @@ class Geeks {
             System.out.println("i is 15");
         else if (i == 20)
             System.out.println("i is 20");
-      
+        else
+            System.out.println("i is not present");
     }
 }
