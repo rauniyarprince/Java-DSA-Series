3a12ff5da08c8b0dc1337b3674cf1f55e5f42330
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-11 22:14:48 +0530

    continue

---
+++ Continue.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 5; i++) {
+            if (i == 2){
+                  System.out.println();
+                  // using continue keyword 
+                  // to skip the current iteration
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
