db6428dabfb05bad1af746df8a63a06e042e3552
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-12 21:57:22 +0530

    break

---
+++ Break.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args)
+    {
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            if (i == 4)
+                break;
+            System.out.println(i);
+        }
+    }
+}
