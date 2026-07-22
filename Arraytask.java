31d46761ae8ad85f2ec93b5aeb789b4d19a6195c
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-22 22:35:54 +0530

    update

--- Arraytask.java
+++ Arraytask.java
@@ -4,7 +4,4 @@ public class Arraytask {
 public class Main {
   public static void main(String[] args) {
     String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-    cars[0] = "Opel";
-    System.out.println(cars[0]);
-  }
-}
+   
