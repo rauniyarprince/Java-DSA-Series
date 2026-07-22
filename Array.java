5a8366fce90aeb44ff831b5fc695e2b3f159218d
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-22 22:32:33 +0530

    array

---
+++ Array.java
@@ -0,0 +1,9 @@
+public class Array {
+  
+}
+public class Main {
+  public static void main(String[] args) {
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    System.out.println(cars[0]);
+  }
+}
