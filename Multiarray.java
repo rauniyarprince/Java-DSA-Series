60fa086f737f9ed6d42e9c503e9c7d7740b8a83a
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-24 22:15:48 +0530

    multidarray

---
+++ Multiarray.java
@@ -0,0 +1,15 @@
+public class Geeks {
+    public static void main(String[] args) {
+
+        // Declaring a 2D array
+        int[][] arr;
+
+        // Initializing row and column sizes
+        arr = new int[1][3];
+
+        // Assigning values
+        arr[0][0] = 3;
+        arr[0][1] = 5;
+        arr[0][2] = 7;
+
+        // Displaying values
