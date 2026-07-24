8eb2f90dc5b9544637a6226d0e52015fb095f3a4
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-24 22:18:15 +0530

    updatemultid

--- multiarrayex.java
+++ multiarrayex.java
@@ -8,3 +8,12 @@ class Main {
 
       	// Array Intialised and Assigned
         int[][] arr = { { 1, 2 }, { 3, 4 } };
+
+      	// Printing the Array
+        for (int i = 0; i < 2; i++){
+            for (int j = 0; j < 2; j++)
+                System.out.print(arr[i][j]+" ");
+          	System.out.println();
+    	}
+    }
+}
