346f659f83918b4706f230b2d56bb5a8600a95dc
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-24 22:17:52 +0530

    exmultidarray

---
+++ multiarrayex.java
@@ -0,0 +1,10 @@
+public class multiarrayex {
+  
+}
+import java.io.*;
+
+class Main {
+    public static void main(String[] args){
+
+      	// Array Intialised and Assigned
+        int[][] arr = { { 1, 2 }, { 3, 4 } };
