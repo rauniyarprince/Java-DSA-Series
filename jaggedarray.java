4357c9fae1a1cbe6936476833e83986acc09a547
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-25 22:15:44 +0530

    update

--- jaggedarray.java
+++ jaggedarray.java
@@ -15,4 +15,9 @@ class Geeks {
                 arr[i][j] = count++;
 
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].
+            for (int j = 0; j < arr[i].length; j++)
+                System.out.print(arr[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
