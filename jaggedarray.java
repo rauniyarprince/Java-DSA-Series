cdb88345ea3997520dbee65abb06198bdcd1ac16
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-25 22:15:16 +0530

    jagged array

---
+++ jaggedarray.java
@@ -0,0 +1,18 @@
+class Geeks {
+    public static void main(String[] args){
+      
+        // Declaring 2-D array with 2 rows
+        int arr[][] = new int[2][];
+
+        // Making the above array Jagged
+        arr[0] = new int[3];
+        arr[1] = new int[2];
+
+        // Initializing array
+        int count = 0;
+        for (int i = 0; i < arr.length; i++)
+            for (int j = 0; j < arr[i].length; j++)
+                arr[i][j] = count++;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].
