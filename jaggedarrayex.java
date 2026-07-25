7294cd63dee487af51939065f5302f8802258b07
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-25 22:18:38 +0530

    ex

--- jaggedarrayex.java
+++ jaggedarrayex.java
@@ -17,3 +17,11 @@ class Geeks {
 
         // Displaying the values of 2D Jagged array
         System.out.println("Contents of 2D Jagged Array");
+      
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++)
+                System.out.print(arr[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
