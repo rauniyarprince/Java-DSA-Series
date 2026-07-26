3d754f52a05ce1f957001b843f1afac7e5fb94cf
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-26 21:55:51 +0530

    aslist

---
+++ ASlist.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Geeks{
+  
+    public static void main(String[] args){
+        // Get the Array
+        int intArr[] = { 10, 20, 15, 22, 35 };
+
+        // To convert the elements as List
+        System.out.println("int Array as List: "
+                           + Arrays.asList(intArr));
+    }
+}
