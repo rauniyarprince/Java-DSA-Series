c1c73b55315a500ed365c8847698294eb122c4ae
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-26 21:59:04 +0530

    binearysearch

---
+++ Binarysearch.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Binarysearch {
+
+    public static void main(String[] args){
+
+        // Get the Array
+        int intArr[] = { 10, 20, 15, 22, 35 };
+
+        Arrays.sort(intArr);
+
+        int intKey = 22;
+
+        // Print the key and corresponding index
+        System.out.println(
+            intKey + " found at index = "
+            + Arrays.binarySearch(intArr, intKey));
+    }
+}
