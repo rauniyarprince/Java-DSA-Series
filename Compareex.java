2b2a1b60ceb2bac1b3f6c7cef82cf8335213dfc1
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-26 22:02:13 +0530

    udate code

--- Compareex.java
+++ Compareex.java
@@ -6,3 +6,12 @@ public class Main {
 
         // Get the Array
         int intArr[] = { 10, 20, 15, 22, 35 };
+
+        // Get the second Array
+        int intArr1[] = { 10, 15, 22 };
+
+        // To compare both arrays
+        System.out.println("int Arrays on comparison: "
+                           + Arrays.compare(intArr, intArr1));
+    }
+}
