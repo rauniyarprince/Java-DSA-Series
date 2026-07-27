b2c5b17dea82dc1cb900e38c560eb602f5bcfd41
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-27 22:09:40 +0530

    object state

---
+++ Objectstate.java
@@ -0,0 +1,10 @@
+class Geeks {
+  
+    int p = 20;
+    public static void main(String args[])
+    {
+        final Geeks t = new Geeks();
+        t.p = 30;
+        System.out.println(t.p);
+    }
+}
