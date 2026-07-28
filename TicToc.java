c8ff9ed918777599487f255f7d7d0608f630eac9
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-28 22:48:10 +0530

    create tictoctoy

---
+++ TicToc.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Geeks {
+
+    static String[] board;
+    static String turn;
+
+    // CheckWinner method will decide the winner
+    static String checkWinner() {
+        for (int a = 0; a < 8; a++) {
+            String line = null;
+
+            switch (a) {
+                case 0:
+                    line = board[0] + board[1] + board[2];
+                    break;
+                case 1:
+                    line = board[3] + board[4] + board[5];
+                    break;
+                case 2:
+                    line = board[6] + board[7] + board[8];
+                    break;
+                case 3:
+                    line = board[0] + board[3] 
