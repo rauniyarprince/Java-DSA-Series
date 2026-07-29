b1e4d41d94bd5b17b4e96c14df72b6106643b8c4
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-07-29 22:36:27 +0530

    methodex

---
+++ MEthodex.java
@@ -0,0 +1,14 @@
+public public class Main {
+  static void myMethod(String fname) {
+    System.out.println(fname + " Refsnes");
+  }
+
+  public static void main(String[] args) {
+    myMethod("Liam");
+    myMethod("Jenny");
+    myMethod("Anja");
+  }
+}
+ {
+  
+}
