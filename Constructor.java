fd9cee2daefdb35362cdda4f94ca33e6a6f5e105
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-08-03 22:39:23 +0530

    update

--- Constructor.java
+++ Constructor.java
@@ -5,3 +5,10 @@ public class Main {
   // Create a class constructor for the Main class
   public Main() {
     x = 5;
+  }
+
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println(myObj.x);
+  }
+}
