254158049360025803bc74915f6514c716529955
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-08-05 21:41:56 +0530

    ex

---
+++ Methodoverloadngex.java
@@ -0,0 +1,13 @@
+public class Main {
+  static int plusMethod(int x, int y) {
+    return x + y;
+  }
+  
+  static double plusMethod(double x, double y) {
+    return x + y;
+  }
+  
+  public static void main(String[] args) {
+    int myNum1 = plusMethod(8, 5);
+    double myNum2 = plusMethod(4.3, 6.26);
+  
