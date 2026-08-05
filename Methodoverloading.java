3133779e583e249ed4d58a979ec55e31e6308dec
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-08-05 21:41:04 +0530

    update

--- Methodoverloading.java
+++ Methodoverloading.java
@@ -10,4 +10,7 @@ public class Main {
   public static void main(String[] args) {
     int myNum1 = plusMethodInt(8, 5);
     double myNum2 = plusMethodDouble(4.3, 6.26);
-  
+    System.out.println("int: " + myNum1);
+    System.out.println("double: " + myNum2);
+  }
+}
