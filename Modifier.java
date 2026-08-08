97fa8d115b7935c69ee99b36671145a0e0f209f5
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-08-08 22:03:50 +0530

    update

--- Modifier.java
+++ Modifier.java
@@ -4,5 +4,9 @@ class Person {
 }
 
 public class Main {
-  p
+  public static void main(String[] args) {
+    Person p = new Person();
+    System.out.println(p.name);   // Works fine
+    System.out.println(p.age);    // Error: age has private access in Person
+  }
 }
