e8ddde7d18a80f103be78bdef6e135c1fade23c7
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-08-11 23:08:57 +0530

    update

--- GFG.java
+++ GFG.java
@@ -5,7 +5,10 @@ public class GFG {
         String s1 = "Computer-";
         String s2 = null;
 
-        //lPointerException
+        // Combining above strings by passing one string as an argument
+        String s3 = s1.concat(s2);
+
+        // It will raise NullPointerException
         System.out.println(s3);
     }
 }
