8fa90d53a8231b8914ebf2f29654af7aec2ae1dc
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-08-11 23:07:59 +0530

    ex

---
+++ Concate2.java
@@ -0,0 +1,11 @@
+public class GFG {
+
+    public static void main(String args[])
+    {
+        String s1 = "Computer-";
+        String s2 = null;
+
+        //lPointerException
+        System.out.println(s3);
+    }
+}
