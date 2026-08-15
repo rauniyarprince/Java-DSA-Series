91a5cdcce15b048247ed73faa1464fa72efede93
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-08-15 22:29:16 +0530

    package

---
+++ Package.java
@@ -0,0 +1,17 @@
+Result Size: 620 x 675
+import java.util.Scanner; // import the Scanner class 
+
+class Main {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    String userName;
+    
+    // Enter username and press Enter
+    System.out.println("Enter username"); 
+    userName = myObj.nextLine();   
+       
+    System.out.println("Username is: " + userName);        
+  }
+}
+
+    
