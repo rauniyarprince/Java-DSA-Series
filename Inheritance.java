19295bb324669b7fdd0b9b2375a189ebb7780357
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-08-15 22:37:29 +0530

    update

--- Inheritance.java
+++ Inheritance.java
@@ -11,4 +11,8 @@ class Vehicle {
 class Car extends Vehicle {
   private String modelName = "Mustang";
   public static void main(String[] args) {
-    
+    Car myFastCar = new Car();
+    myFastCar.honk();
+    System.out.println(myFastCar.brand + " " + myFastCar.modelName);
+  }
+}
