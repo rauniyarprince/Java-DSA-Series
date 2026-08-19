24a9ed445277c9ec6b51caffb502c6c80c0ef7ee
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-08-19 20:14:56 +0530

    interface

---
+++ Interface.java
@@ -0,0 +1,8 @@
+interface Animal {
+  public void animalSound(); // interface method (does not have a body)
+  public void sleep(); // interface method (does not have a body)
+}
+
+class Pig implements Animal {
+  public void animalSound() {
+ 
