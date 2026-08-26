4e8dde079b870ec98801fb26ffabb2446ee71d65
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-08-26 12:53:56 +0530

    updated

--- Exceptipn2.java
+++ Exceptipn2.java
@@ -3,4 +3,8 @@ public class Main {
     try {
       int[] myNumbers = {1, 2, 3};
       System.out.println(myNumbers[10]);
-    } catch (Exc
+    } catch (Exception e) {
+      System.out.println("Something went wrong.");
+    }
+  }
+}
