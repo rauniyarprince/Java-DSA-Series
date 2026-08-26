6a8f1041f8e82143f4c06192cd8cf93d6081586d
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-08-26 12:54:56 +0530

    update

--- Concate.java
+++ Concate.java
@@ -10,10 +10,6 @@ public class GFG {
         String s3 = s1.concat(s2);
 
         // Print and display temporary combined string
-        System.out.println(s3);
-
-        String s4 = "Portal";
-        String s5 = s3.concat(s4);
-        System.out.println(s5);
+        Syst);
     }
 }
