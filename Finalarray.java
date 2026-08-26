0fcf5f9bf360b4cebe12085e48144a647ad4004b
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-08-26 12:54:14 +0530

    update

--- Finalarray.java
+++ Finalarray.java
@@ -4,10 +4,6 @@ class Geeks {
     public static void main(String[] args)
     {
 
-        final int[] arr = {10,20,30};
-
-        arr[2] = 99;
-
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
