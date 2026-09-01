9e6f62b896661f4b92bcd47f354e3d24748f9a9e
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-09-01 23:00:15 +0530

    update

--- Polymorphism.java
+++ Polymorphism.java
@@ -17,5 +17,13 @@ class Dog extends Animal {
 }
 
 class Main {
-  
+  public static void main(String[] args) {
+    Animal myAnimal = new Animal();
+    Animal myPig = new Pig();
+    Animal myDog = new Dog();
+        
+    myAnimal.animalSound();
+    myPig.animalSound();
+    myDog.animalSound();
+  }
 }
