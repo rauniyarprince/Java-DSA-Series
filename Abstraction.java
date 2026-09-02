4a8661717295470ae1090da327a333ac2c199719
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-09-02 23:04:35 +0530

    update

--- Abstraction.java
+++ Abstraction.java
@@ -11,4 +11,10 @@ abstract class Animal {
 // Subclass (inherit from Animal)
 class Pig extends Animal {
   public void animalSound() {
- 
+    // The body of animalSound() is provided here
+    System.out.println("The pig says: wee wee");
+  }
+}
+
+cla
+}
