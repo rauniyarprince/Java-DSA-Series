cdfdc885447883ae698f9ef33611bcd2e14b8b6b
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-09-02 23:02:53 +0530

    abstraction

---
+++ Abstraction.java
@@ -0,0 +1,14 @@
+// Abstract class
+abstract class Animal {
+  // Abstract method (does not have a body)
+  public abstract void animalSound();
+  // Regular method
+  public void sleep() {
+    System.out.println("Zzz");
+  }
+}
+
+// Subclass (inherit from Animal)
+class Pig extends Animal {
+  public void animalSound() {
+ 
