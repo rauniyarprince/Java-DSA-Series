c67ee173eb6c9ee2a057b03357cb1cbd0a431a7c
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-09-05 22:44:11 +0530

    update

--- Userinput2.java
+++ Userinput2.java
@@ -16,4 +16,8 @@ class Main {
     // Output input by user
     System.out.println("Name: " + name);
     System.out.println("Age: " + age);
+    System.out.println("Salary: " + salary);
+  }
+} {
   
+}
