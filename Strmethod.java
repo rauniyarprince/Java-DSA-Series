62f6bfa68bea8d1ad64aee5f6f8555f70b88ea11
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-09-09 23:19:20 +0530

    str method

---
+++ Strmethod.java
@@ -0,0 +1,9 @@
+public class Geeks{
+    public static void main(String[] args) {
+        String str = "GeeksforGeeks";
+
+        System.out.println("Length: " + str.length());  
+        System.out.println("Uppercase: " + str.toUpperCase());
+        System.out.println("Substring: " + str.substring(2, 6));
+    }
+}
